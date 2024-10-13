c4c97dec2482ac822074eaf15bfaede0865897f1
do-one-30 <rnjsehdnjs30@gmail.com>
2024-10-13 00:42:22 +0000

    순서 쬐끔 변화

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -119,8 +119,8 @@ sema_up (struct semaphore *sema)
   } 
     
   sema->value++;
-  thread_swap();
   intr_set_level (old_level);
+  thread_swap();
 }
 
 static void sema_test_helper (void *sema_);
